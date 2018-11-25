9fb744fbc38b1cc54cdc73928657f596e70e185b
John Kusching <kimshrum@Kims-Air.home>
2018-11-24 20:53:02 -0600

    Updated with exercise 3

---
+++ L03/thrid.py
@@ -0,0 +1,19 @@
+try:
+    score = float(input("Enter Score: "))
+except:
+    score = 10.0
+
+if (score > 1.0) or (score < 0.0):
+    print("Invalid score")
+else:
+    if score >= 0.9:
+        print("A")
+    elif score >= 0.8:
+        print("B")
+    elif score >= 0.7:
+        print("C")
+    elif score >= 0.6:
+        print("D")
+    else:
+        print("F")
+        
