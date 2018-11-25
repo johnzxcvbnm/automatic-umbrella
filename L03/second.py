d2ac6067557ab34f022150cf5fc73885e2492e9d
John Kusching <kimshrum@Kims-Air.home>
2018-11-24 21:16:21 -0600

    Updated second.py to include try and except

--- L03/second.py
+++ L03/second.py
@@ -1,8 +1,18 @@
-h = float(input("Enter Hours:"))
-r = float(input("Enter rate:"))
+try:
+    h = float(input("Enter Hours: "))
+except:
+    print("Error!  You get no hours!")
+    h = 0
+
+try:
+    r = float(input("Enter rate: "))
+except:
+    print("Error! You work for free!")
+    r = 0
+
 pay = h * r
 
 if h > 40 :
     pay += (h - 40) * (0.5 * r)
 
-print(pay)
+print("Pay:", pay)
